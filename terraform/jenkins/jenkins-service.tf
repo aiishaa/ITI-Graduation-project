9a658d18c073ff9efbbcbba704bc7fc04220e859
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 01:24:00 +0300

    Exposed port 50000 on the Jenkins master service

--- terraform/jenkins/jenkins-service.tf
+++ terraform/jenkins/jenkins-service.tf
@@ -13,9 +13,17 @@ resource "kubernetes_service" "jenkins" {
     }
 
     port {
+      name        = "manager"
       port        = 8080
       target_port = 8080
       node_port   = 32003
+    }
+    port {
+      port        = 50000
+      target_port = 50000
+      node_port   = 32006
+      protocol    = "TCP"
+      name        = "agent"
     }
 
     type = "NodePort"
