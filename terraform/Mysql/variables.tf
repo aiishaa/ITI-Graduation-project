4349dbc4a4023b170a8aeca331708e1ac2511704
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 14:32:46 +0300

    Create variables.tf

---
+++ terraform/Mysql/variables.tf
@@ -0,0 +1,23 @@
+variable "mysql_root_password" {
+  type        = string
+  description = "The root password for MySQL."
+  sensitive   = true
+}
+
+variable "mysql_user_password" {
+  type        = string
+  description = "The user password for MySQL."
+  sensitive   = true
+}
+
+variable "mysql_username" {
+  type        = string
+  description = "The username for MySQL."
+  sensitive   = true
+}
+
+variable "mysql_database_name" {
+  type        = string
+  description = "The database name for MySQL."
+  sensitive   = true
+}
