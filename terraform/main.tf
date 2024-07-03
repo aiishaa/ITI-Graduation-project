33308acc3a473e05fe18fdc57680464eb0222ac6
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 17:00:58 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,9 @@ module "namespaces" {
 
 module "nexus" {
   source = "./nexus"
+
+  docker_username = var.docker_username
+  docker_password = var.docker_password
 }
 
 module "jenkins" {
@@ -22,9 +25,13 @@ module "jenkins" {
 
 module "MySQL" {
   source = "./Mysql"
+
+  mysql_root_password = var.mysql_root_password
+  mysql_user_password = var.mysql_user_password
+  mysql_username      = var.mysql_username
+  mysql_database_name = var.mysql_database_name
 }
 
 module "nodejs" {
   source = "./nodeapp"
 }
-
