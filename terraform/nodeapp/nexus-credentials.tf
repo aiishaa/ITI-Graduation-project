e2b896f2aeec004215272ebf911469ff0fa5d8c1
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 14:37:28 +0300

    Update nexus-credentials.tf

--- terraform/nodeapp/nexus-credentials.tf
+++ terraform/nodeapp/nexus-credentials.tf
@@ -1,1 +1,20 @@
+resource "kubernetes_secret" "nexus-cred" {
+  metadata {
+    name      = "docker-registry-secret"
+    namespace = kubernetes_namespace.dev.metadata[0].name
+  }
 
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = base64encode(jsonencode({
+      auths = {
+        "${var.docker_server}" = {
+          username = var.docker_username
+          password = var.docker_password
+          auth     = base64encode("${var.docker_username}:${var.docker_password}")
+        }
+      }
+    }))
+  }
+}
