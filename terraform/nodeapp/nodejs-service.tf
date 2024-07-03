0de07f71e534bbc21c1484fdadca648347797c7c
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 14:38:56 +0300

    Update nodejs-service.tf

--- terraform/nodeapp/nodejs-service.tf
+++ terraform/nodeapp/nodejs-service.tf
@@ -1,1 +1,20 @@
+resource "kubernetes_service" "nodejs" {
+  metadata {
+    name      = "nodejs"
+    namespace = kubernetes_namespace.dev.metadata[0].name
+  }
 
+  spec {
+    selector = {
+      app = "nodejs"
+    }
+
+    port {
+      port        = 3000
+      target_port = 3000
+      node_port   = 32005
+    }
+
+    type = "NodePort"
+  }
+}
