0bba454d412b3b9318f26eb08845510bcae587b1
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 14:33:48 +0300

    Update mysql-service.tf

--- terraform/Mysql/mysql-service.tf
+++ terraform/Mysql/mysql-service.tf
@@ -1,1 +1,18 @@
 
+resource "kubernetes_service" "mysql" {
+  metadata {
+    name      = "mysql"
+    namespace = kubernetes_namespace.dev.metadata[0].name
+  }
+
+  spec {
+    port {
+      port        = 3306
+      target_port = 3306
+    }
+    cluster_ip = "None"
+    selector = {
+      app = "mysql"
+    }
+  }
+}
