218586482c17ac5cc9f4165de39a9de09a9aee01
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 16:57:16 +0300

    Update nexus-deploy.tf

--- terraform/nexus/nexus-deploy.tf
+++ terraform/nexus/nexus-deploy.tf
@@ -1,36 +1,10 @@
-resource "kubernetes_deployment" "nexus" {
-  metadata {
-    name      = "nexus-deploy"
-    namespace = "tools"
-    labels = {
-      deploy = "nexus"
-    }
-  }
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = "nexus"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "nexus"
-        }
-      }
-      spec {
-        container {
-          image = "sonatype/nexus3"
-          name  = "nexus"
-          port {
-            container_port = 8081
-          }
-          port {
-            container_port = 8083
-          }
-        }
-      }
-    }
-  }
+variable "docker_username" {
+  type        = string
+  description = "Docker registry username"
+}
+
+variable "docker_password" {
+  type        = string
+  description = "Docker registry password"
+  sensitive   = true
 }
