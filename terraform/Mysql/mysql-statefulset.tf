9158ecf58616ca52f30a5ed0fb9f1c1f961729b4
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-03 17:00:08 +0300

    Update mysql-statefulset.tf

--- terraform/Mysql/mysql-statefulset.tf
+++ terraform/Mysql/mysql-statefulset.tf
@@ -1,7 +1,66 @@
+resource "kubernetes_secret" "mysql_secret" {
+  metadata {
+    name      = "mysql-secret"
+    namespace = "dev"
+  }
+
+  data = {
+    root-password = base64encode(var.mysql_root_password)
+    user-password = base64encode(var.mysql_user_password)
+    username      = base64encode(var.mysql_username)
+    database-name = base64encode(var.mysql_database_name)
+  }
+}
+ubuntu@ip-172-31-234-205:~/iti-project/terraform/Mysql$ ls
+mysql-secret.tf  mysql-service.tf  mysql-statefulset.tf  variables.tf
+ubuntu@ip-172-31-234-205:~/iti-project/terraform/Mysql$ cat mysql-service.tf 
+
+resource "kubernetes_service" "mysql" {
+  metadata {
+    name      = "mysql"
+    namespace = "dev"
+  }
+
+  spec {
+    port {
+      port        = 3306
+      target_port = 3306
+    }
+    cluster_ip = "None"
+    selector = {
+      app = "mysql"
+    }
+  }
+}
+ubuntu@ip-172-31-234-205:~/iti-project/terraform/Mysql$ cat variables.tf 
+variable "mysql_root_password" {
+  type        = string
+  description = "The root password for MySQL."
+  sensitive   = true
+}
+
+variable "mysql_user_password" {
+  type        = string
+  description = "The user password for MySQL."
+  sensitive   = true
+}
+
+variable "mysql_username" {
+  type        = string
+  description = "The username for MySQL."
+  sensitive   = true
+}
+
+variable "mysql_database_name" {
+  type        = string
+  description = "The database name for MySQL."
+  sensitive   = true
+}
+ubuntu@ip-172-31-234-205:~/iti-project/terraform/Mysql$ cat mysql-statefulset.tf 
 resource "kubernetes_stateful_set" "mysql" {
   metadata {
     name      = "mysql"
-    namespace = kubernetes_namespace.dev.metadata[0].name
+    namespace = "dev"
   }
 
   spec {
