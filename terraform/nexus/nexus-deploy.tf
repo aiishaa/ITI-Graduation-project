d22345361eb6110803d27d2ff390ceae408fafdd
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-07-05 13:41:24 +0300

    added pv and pvc for nexus

--- terraform/nexus/nexus-deploy.tf
+++ terraform/nexus/nexus-deploy.tf
@@ -1,3 +1,34 @@
+resource "kubernetes_persistent_volume" "nexus_pv" {
+  metadata {
+    name = "nexus-pv"
+  }
+  spec {
+    capacity = {
+      storage = "10Gi"
+    }
+    access_modes = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    host_path {
+      path = "/mnt/data/nexus"
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "nexus_pvc" {
+  metadata {
+    name      = "nexus-pvc"
+    namespace = "tools"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+  }
+}
+
 resource "kubernetes_deployment" "nexus" {
   metadata {
     name      = "nexus-deploy"
@@ -28,6 +59,16 @@ resource "kubernetes_deployment" "nexus" {
           }
           port {
             container_port = 8083
+          }
+          volume_mount {
+            mount_path = "/nexus-data"
+            name       = "nexus-storage"
+          }
+        }
+        volume {
+          name = "nexus-storage"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.nexus_pvc.metadata[0].name
           }
         }
       }
